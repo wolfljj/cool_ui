5cdbf4e8ab8ffd4c92a5d638fd4ba1fc8fb9739d
wolfljj <wolfljj007@qq.com>
2021-03-05 17:47:58 +0800

    Update weui_toast.dart

--- lib/dialogs/weui_toast.dart
+++ lib/dialogs/weui_toast.dart
@@ -137,7 +137,7 @@ class WeuiToastConfig extends InheritedWidget{
 
   
   static WeuiToastConfigData of(BuildContext context) {
-    var widget = context.dependOnInheritedWidgetOfExactType(WeuiToastConfig);
+    var widget = context.dependOnInheritedWidgetOfExactType(aspect:WeuiToastConfig);
     if(widget is WeuiToastConfig){
       return widget.data;
     }
